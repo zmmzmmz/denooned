3b5b822685eb29062cfab2c799bec9b06f4c2d00
pdan <petterstrong007@gmail.com>
2022-06-11 02:51:41 +0800

    feat: update template

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
 import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
-import {configure} from "./mod/eta/mod.ts";
+// import {configure} from "./mod/eta/mod.ts";
 import PageRoute from './routes/page.ts';
 import WebRoute from './routes/web.ts';
 
 const app = new Application();
 
-const viewPath = `${Deno.cwd()}/templates/`
-configure({
-  views: viewPath,
-  tags: ['{{', '}}'],
-  async: true
-})
+// const viewPath = `${Deno.cwd()}/templates/`
+// configure({
+//   views: viewPath,
+//   tags: ['{{', '}}'],
+//   async: true
+// })
 
 // api routes
 app.use(PageRoute.routes());
--- routes/web.ts
+++ routes/web.ts
@@ -1,17 +1,18 @@
 import { Router } from "https://deno.land/x/oak@v10.6.0/mod.ts";
-import {renderFile} from "../mod/eta/mod.ts";
+// import {renderFile} from "../mod/eta/mod.ts";
+import { renderFile } from "https://deno.land/x/mustache@v0.3.0/mod.ts";
 
 const router = new Router();
 
 // 首页
 router.get('/', async (ctx) => {
-  const res = await renderFile('./index.html', {}) || '';
+  const res = await renderFile(`${Deno.cwd()}/templates/index.html`, {}) || '';
   ctx.response.body = res;
 });
 
 // 容器路径
 router.get('/page/:path', async (ctx) => {
-  const res = await renderFile('./container.html', { name: 903 }) || '';
+  const res = await renderFile(`${Deno.cwd()}/templates/container.html`, { name: 903 }) || '';
   ctx.response.body = res;
 })
 
